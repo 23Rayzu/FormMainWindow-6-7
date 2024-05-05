43b5c6dededb4ddaf0f7832cea573ce0d8f1b5de
23Rayzu <160707228+23Rayzu@users.noreply.github.com>
2024-05-06 05:00:31 +0700

    Add files via upload

---
+++ FormMainWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PGD_6_7
+{
+    public partial class FormMainWindow : Form
+    {
+        public FormMainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
